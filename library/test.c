f7a191fb8f51a532b7834e4bbaea3507a18779a4
billy.lin <billy.lin@emplustech.com>
2025-03-13 16:22:27 +0800

    Add uclibc-ng

---
+++ library/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*** --------------------------------------------------***/
+
+/*** --------------------------------------------------***/
+
+int main(int argc, char *argv[]) 
+{
+    printf ("Hello World!\r\n");
+	return 0;
+}
